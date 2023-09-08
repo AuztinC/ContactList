3fd8f925a6cfc06b108c7d665496e1c4fef112fe
Austin Cripe <aaustinc@live.com>
2023-09-07 23:40:05 -0400

    nice list

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  const [hash, setHash] = useState(window.location.hash.slice(1)*1)
+  useEffect(()=>{
+    fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users')
+      .then(response => response.json())
+      .then(json => setUsers(json))
+  }, [])
 
+  useEffect(()=> {
+    window.addEventListener("hashchange", ()=>{
+      setHash(window.location.hash.slice(1)*1)
+    })
+  }, [])
+
+  const user = users.find( user => user.id === hash)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1> Today we have { users.length } Users! </h1>
+      <ul>
+        {
+          users.map( user => {
+            return (
+              <li key={ user.id } className={ hash === user.id ? "selected" : null }>
+                <a href={`#${user.id === hash ? "" : user.id}`}>
+                  { user.name }
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+
+      {
+        user ? <div>
+          { user.email } <br></br>
+          { user.company.name } <br></br>
+          { user.phone }
+        </div> : null
+      }
     </>
   )
 }
